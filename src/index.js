46d2b042a5d36b8985bfaf9835fa15d77781397c
tirthamoulib <btirthamouli@gmail.com>
2021-01-23 14:14:08 +0530

    Created start point

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
 import './style/util.css';
 import './style/style.css';
-import Board from './components/board';
-
-const board = new Board();
-document.querySelector('#main').appendChild(board.render());
-document.querySelector('#solve').addEventListener('click', board.setUpBeforeSolveAndSolve);
-document.querySelector('#speed').addEventListener('input', board.handleSpeedInput);
-document.querySelector('#addArrayButton').addEventListener('click',
-  () => board.handleInputFromArray(document.querySelector('#arrayInput').value));
